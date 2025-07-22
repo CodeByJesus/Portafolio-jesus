464f3a23d82b7f01b0c8ab1987d64317bb018a32
CodeByJesus <jd67941@gmail.com>
2025-07-22 16:26:36 -0500

    Fix: Robust DATABASE_URL handling in settings

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'portafolio.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
+        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
         conn_max_age=600
     )
 }
