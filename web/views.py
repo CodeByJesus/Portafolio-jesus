5f4e3ac3b1c702e22c6038f80bcb2407d4604053
CodeByJesus <jd67941@gmail.com>
2025-07-22 18:25:19 -0500

    Feat: Implement i18n for project descriptions and technologies

--- web/views.py
+++ web/views.py
@@ -1,6 +1,17 @@
 from .models import Proyecto
 from django.shortcuts import render
+from django.utils.translation import get_language
 
 def inicio(request):
     proyectos = Proyecto.objects.all()
+    current_language = get_language()
+
+    for proyecto in proyectos:
+        if current_language == 'en' and proyecto.descripcion_en and proyecto.tecnologias_en:
+            proyecto.display_descripcion = proyecto.descripcion_en
+            proyecto.display_tecnologias = proyecto.tecnologias_en
+        else:
+            proyecto.display_descripcion = proyecto.descripcion
+            proyecto.display_tecnologias = proyecto.tecnologias
+
     return render(request, 'inicio.html', {'proyectos' : proyectos})
